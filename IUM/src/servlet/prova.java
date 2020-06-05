98b30365b881cf8bcb77e4edb76364184d06f01d
Oliver1837 <alessandro.capodanno95@gmail.com>
2020-06-05 14:09:22 +0200

    Prova email.Ho inserito una Servlet prova e jar .

--- IUM/src/servlet/prova.java
+++ IUM/src/servlet/prova.java
@@ -1,6 +1,12 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.activation.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,17 +33,61 @@ public class prova extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		Connessione con=new Connessione();
-		con.getConn();
-		
-		
-		
-		con.closeConn();
-		
-		
-		response.getWriter().append("ok: ").append(request.getContextPath());
-		
-	
+		  // Recipient's email ID needs to be mentioned.
+	      String to = "alessandro.capodanno95@gmail.com";
+	 
+	      // Sender's email ID needs to be mentioned
+	      String from = "a.capodanno5@studenti.unisa.it";
+	 
+	      // Assuming you are sending email from localhost
+	      String host = "localhost";
+	 
+	      // Get system properties
+	      Properties properties = System.getProperties();
+	 
+	      // Setup mail server
+	      properties.setProperty("mail.smtp.host", host);
+	 
+	      // Get the default Session object.
+	      Session session = Session.getDefaultInstance(properties);
+	      
+	      // Set response content type
+	      response.setContentType("text/html");
+	      PrintWriter out = response.getWriter();
+
+	      try {
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+	         
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+	         
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	         
+	         // Set Subject: header field
+	         message.setSubject("This is the Subject Line!");
+	         
+	         // Now set the actual message
+	         message.setText("This is actual message");
+	         
+	         // Send message
+	         Transport.send(message);
+	         String title = "Send Email";
+	         String res = "Sent message successfully....";
+	         String docType =
+	            "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+	         
+	         out.println(docType +
+	            "<html>\n" +
+	               "<head><title>" + title + "</title></head>\n" +
+	               "<body bgcolor = \"#f0f0f0\">\n" +
+	                  "<h1 align = \"center\">" + title + "</h1>\n" +
+	                  "<p align = \"center\">" + res + "</p>\n" +
+	               "</body></html>");
+	      } catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
 		
 	}
 
