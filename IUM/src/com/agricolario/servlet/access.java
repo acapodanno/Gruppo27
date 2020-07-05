d51a1a525a6aefd13faf3c84edff99675a7e9c07
Oliver1837 <alessandro.capodanno95@gmail.com>
2020-07-05 17:34:01 +0200

    sadfasd

--- IUM/src/com/agricolario/servlet/access.java
+++ IUM/src/com/agricolario/servlet/access.java
@@ -40,7 +40,7 @@ public class access extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//ok
+		//okf
 		String mannaggia="aMaron";
 		String access= request.getParameter("access");
 		String email,password,nome,cognome,dataNascita,ruolo,nomeAzienda,indirizzo,città,cap,dataFondazione;
